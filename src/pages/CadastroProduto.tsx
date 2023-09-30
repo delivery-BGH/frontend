30c25323dbb1762a405b62a5916b0b333b6cedb2
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-09-30 18:59:49 -0300

    lindo

--- src/pages/CadastroProduto.tsx
+++ src/pages/CadastroProduto.tsx
@@ -1,20 +1,21 @@
-import { useState } from "react";
-import { CATEGORIES } from "../entities/AddCategory";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod/dist/zod.js";
+import { Input } from "@/components/ui/input";
+import { Checkbox } from "@/components/ui/checkbox"
 
 export const CadastroProdutos = () => {
-  const [item, setItem] = useState({ category: "" }); // Inicialize o estado com um objeto que contém a propriedade 'category'
+  const [prom, setProm] = useState(false)
+  const [card, setCard] = useState(false)
 
   const createUserFormSchema = z.object({
     nome: z.string().nonempty("Nome obrigatório"),
     preco: z.string().nonempty("Obrigatório"),
     descricao: z.string().nonempty("Obrigatório"),
     precoProm: z.string().nonempty("Obrigatório"),
-    categoria: z.string().refine((value) => CATEGORIES.includes(value), {
-      message: "Categoria inválida",
-    }),
+    categoria: z.string(),
     url: z.string().refine(
       (value) => {
         // Adicione sua lógica de validação personalizada para a URL aqui
@@ -26,8 +27,6 @@ export const CadastroProdutos = () => {
         message: "URL inválida",
       }
     ),
-    ativarProm: z.boolean(),
-    ativarCard: z.boolean()
   });
 
   const {
@@ -39,82 +38,86 @@ export const CadastroProdutos = () => {
     mode: "all",
     criteriaMode: "all",
   });
+  
   type createUserFormData = z.infer<typeof createUserFormSchema>;
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const handleCategoryChange = (ev: any) => {
-    // Atualize o estado 'item' com o valor selecionado
-    setItem({ ...item, category: ev.target.value });
-  };
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   function createProduto(data: any) {
-    console.log(data);
+    const x = { ...data, prom, card }
+    console.log("enviado para o banco: ", x)
   }
+
+  const [categorias, setCategorias] = useState<any>()
+  useEffect(() => {
+    getCategorias()
+
+  }, [])
+
+  const getCategorias = async () => {
+    const response = await fetch('http://localhost:3000/categorias')
+    const countries = await response.json()
+    setCategorias(countries)
+  }
+
 
   return (
     <>
       <form onSubmit={handleSubmit(createProduto)}>
         <div>
           <label htmlFor="nome">Nome:</label>
-          <input type="text" {...register("nome")} />
+          <Input type="text" {...register("nome")} />
           {errors.nome && <span>{errors.nome.message}</span>}
         </div>
         <div>
           <label htmlFor="nome">Preço:</label>
-          <input type="number" {...register("preco")} />
+          <Input type="number" {...register("preco")} />
           {errors.preco && <span>{errors.preco.message}</span>}
         </div>
         <div>
           <label htmlFor="nome">Descrição:</label>
-          <input type="text" {...register("descricao")} />
+          <Input type="text" {...register("descricao")} />
           {errors.descricao && <span>{errors.descricao.message}</span>}
         </div>
         <div>
           <label htmlFor="nome">Url da imagem</label>
-          <input type="text" {...register("url")} />
+          <Input type="text" {...register("url")} />
         </div>
         <div>
           <label htmlFor="nome">Preço Promocional:</label>
-          <input type="number" {...register("precoProm")} />
+          <Input type="number" {...register("precoProm")} />
           {errors.precoProm && <span>{errors.precoProm.message}</span>}
         </div>
         <div>
           <label htmlFor="category">Categoria</label>
           <select
+            className="bg-background"
             id="category"
-            required
             {...register("categoria")}
-            value={item.category}
-            onChange={handleCategoryChange}
+          // value={category.name}
           >
             <option disabled value="">
               Selecione uma categoria...
             </option>
-            {CATEGORIES.map((category) => (
+
+            {categorias && categorias.map((category: any, index: number) => (
               <option
-                key={category}
-                value={category}
-                defaultChecked={item.category === category}
+                className="bg-background"
+                key={index}
+                value={category.nome}
               >
-                {category}
+                {category.nome}
               </option>
             ))}
           </select>
         </div>
+        <span>{errors.categoria?.message}</span>
         <div>
           <h4>Ativar promoção</h4>
-          <label className="relative inline-flex items-center cursor-pointer">
-            <input className="sr-only peer" value="" type="checkbox" {...register("ativarProm")}/>
-            <div className=" peer ring-2 ring-gray-500 bg-gradient-to-r from-rose-400 to-red-900 rounded-full outline-none duration-500 after:duration-300 w-20 h-5  shadow-inner peer-checked:bg-gradient-to-r peer-checked:from-emerald-500 peer-checked:to-emerald-900 shadow-gray-900 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-gray-500  after:content-[''] after:rounded-full after:absolute after:outline-none after:h-9 after:w-9 after:bg-gray-900 after:-top-2 after:-left-2 after:flex after:justify-center after:items-center after:border-4 after:border-gray-500  peer-checked:after:translate-x-14"></div>
-          </label>
+          <Checkbox onClick={() => { setProm(!prom) }} />
         </div>
         <div className="">
           <h4>Liberar no Cardápio</h4>
-          <label className="relative inline-flex items-center cursor-pointer">
-            <input className="sr-only peer" value="" type="checkbox" {...register("ativarCard")}/>
-            <div className=" peer ring-2 ring-gray-500 bg-gradient-to-r from-rose-400 to-red-900 rounded-full outline-none duration-500 after:duration-300 w-20 h-5  shadow-inner peer-checked:bg-gradient-to-r peer-checked:from-emerald-500 peer-checked:to-emerald-900 shadow-gray-900 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-gray-500  after:content-[''] after:rounded-full after:absolute after:outline-none after:h-9 after:w-9 after:bg-gray-900 after:-top-2 after:-left-2 after:flex after:justify-center after:items-center after:border-4 after:border-gray-500  peer-checked:after:translate-x-14"></div>
-          </label>
+          <Checkbox onClick={() => { setCard(!card) }} />
         </div>
         <button
           type="submit"
