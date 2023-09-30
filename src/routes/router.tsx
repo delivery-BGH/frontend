51d414c4badef1890360fbea7fb9e11b7301003c
HugoNovais <hugo.o.novais@outlook.com>
2023-09-30 17:33:26 -0300

    PaginasCorrigidas
    Paginas Corrigidas

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,9 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Menu from "../components/Menu";
+import { CadastroProdutos } from "@/pages/CadastroProduto";
+import CadastroCliente from "@/pages/CadastroCliente";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Menu />,
   },
+  {
+    path: "/cadastroProduto",
+    element: <CadastroProdutos/>
+  },
+  {
+    path: "/CadastroCliente",
+    element: <CadastroCliente />
+  }
 ]);
