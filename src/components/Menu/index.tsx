164a8dbb7e7a9c5884b39c650085fbf667c47abf
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-09-30 17:36:44 -0300

    menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -5,10 +5,8 @@ export default function Menu() {
   return (
     <>
       <ul>
-        <li><a href="/componentes">Componentes</a></li>
-        <li><a href="/login">Login</a></li>
-        <li><a href="/CadastroCliente">Cadastro de Cliente</a></li>
-        <li><a href="/CadastroProduto">Cadastro de Produto</a></li>
+        <li><a href="/cadastroProduto">Cadastro Produto</a></li>
+        <li><a href="/CadastroCliente">Cadastro Cliente</a></li>
       </ul >
 
       <div className="flex flex-row gap-2 self-end">
