5f158e41984dfcb4f6a27506217e55631f7dc8d9
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-09-30 12:31:20 -0300

    shadcn instalado

---
+++ src/Context/Language/LanguageContext.tsx
@@ -0,0 +1,11 @@
+
+import { TFunction } from "i18next";
+import { createContext } from "react";
+
+export type LanguageContextType = {
+  t: TFunction<"translation", undefined>;
+  currentlanguage: string;
+  handleChangeLanguage: () => void;
+};
+
+export const LanguageContext = createContext<LanguageContextType>(null!);
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import '../src/styles/global.css'
 import { RouterProvider } from "react-router-dom";
-import { router } from './router.tsx';
+import { router } from './routes/router.tsx';
+import './lib/i18n'
+import { ThemeProvider } from './Context/Theme/ThemeContext.tsx';
+import { LanguageProvider } from './Context/Language/LanguageProvider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <LanguageProvider>
+        <RouterProvider router={router} />
+      </LanguageProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 )
