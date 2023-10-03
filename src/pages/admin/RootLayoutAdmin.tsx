39c29ba2ec4868e2ea82ca1cc8e67f171b7c0d1d
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-10-02 22:26:44 -0300

    XD

--- src/pages/admin/RootLayoutAdmin.tsx
+++ src/pages/admin/RootLayoutAdmin.tsx
@@ -3,9 +3,9 @@ import { Link, Outlet } from "react-router-dom";
 
 export default function RootLayoutAdmin() {
   return (
-    <div className="p-6">
+    <div className="p-6 w-full">
       <header className="flex flex-row w-full justify-between">
-        <div className="border-2 w-full flex flex-row justify-between">
+        <div className="w-full flex flex-row justify-between">
           <Link to="/" className="logo">Delivery BGH</Link>
           <nav className="flex flex-row gap-2">
             <Link to="/">Início</Link>
