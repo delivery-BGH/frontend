a5380488da53e9a10ffff4e2f79d93aef70a6079
HugoNovais <hugo.o.novais@outlook.com>
2023-10-08 16:43:51 -0300

    Comitas

--- src/pages/admin/Update.tsx
+++ src/pages/admin/Update.tsx
@@ -1,8 +1,15 @@
+/*import { Input } from "@/components/ui/input";
+import { useCreateProduto } from "@/forms/CadastroProduto/useCadastroProduto";
+import { Checkbox } from "@radix-ui/react-checkbox";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+*/
+export function UpdateProduto() {
 
-export function UpdateProduto(){
   return (
     <>
-    <h2>Atualizar Produto</h2>
-    </>
-  )
+    <h2>teste</h2></>
+  );
 }
+
